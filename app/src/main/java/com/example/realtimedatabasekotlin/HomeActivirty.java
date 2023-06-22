6324a981849a29439f260d69f68c6cd017599e05
Faizhaidar <faizchaudhary110@gmail.com>
2023-06-22 14:19:33 +0530

    show data on recyclerview is not complate commite

--- app/src/main/java/com/example/realtimedatabasekotlin/HomeActivirty.java
+++ app/src/main/java/com/example/realtimedatabasekotlin/HomeActivirty.java
@@ -1,32 +1,27 @@
 package com.example.realtimedatabasekotlin;
 
-import static androidx.constraintlayout.widget.Constraints.TAG;
-
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.annotation.SuppressLint;
-import android.nfc.Tag;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.google.firebase.FirebaseApp;
 import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.logging.LogManager;
+import java.util.ArrayList;
+import java.util.List;
 
 public class HomeActivirty extends AppCompatActivity {
 
@@ -37,30 +32,33 @@ public class HomeActivirty extends AppCompatActivity {
     private DatabaseReference mRef;
     private ChildEventListener mChildeListner;
 
+    private RecyclerView mRecyclerView;
+    private UserAdapter mUserAdapter;
+    private List<User> mDataList;
+
     @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_activirty);
 
+        initView();
+
         mDatabase = FirebaseDatabase.getInstance();
         mRef = mDatabase.getReference("users");
 
-        btSubmitButton = findViewById(R.id.btSubmitButton);
-        btReadData = findViewById(R.id.btReadData);
-
-        mOutputText = findViewById(R.id.mOutputText);
-        mInpurtName = findViewById(R.id.mInpurtName);
-        mInpurtAge = findViewById(R.id.mInpurtAge);
-
-
         btSubmitButton.setOnClickListener(this::runcode);
         btReadData.setOnClickListener(this::readCode);
+        mDataList = new ArrayList<>();
 
         mChildeListner = new ChildEventListener() {
             @Override
             public void onChildAdded(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
                 //Person person = snapshot.getValue(Person.class);
+
+                User user = snapshot.getValue(User.class);
+                mDataList.add(user);
+                mUserAdapter.notifyDataSetChanged();
             }
 
             @Override
@@ -84,8 +82,22 @@ public class HomeActivirty extends AppCompatActivity {
             }
         };
         mRef.addChildEventListener(mChildeListner);
+
+        //Item RecyclerView
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+        mUserAdapter = new UserAdapter(this,mDataList);
+        mRecyclerView.setAdapter(mUserAdapter);
     }
 
+    public void initView(){
+        btSubmitButton = findViewById(R.id.btSubmitButton);
+        btReadData = findViewById(R.id.btReadData);
+
+        mOutputText = findViewById(R.id.mOutputText);
+        mInpurtName = findViewById(R.id.mInpurtName);
+        mInpurtAge = findViewById(R.id.mInpurtAge);
+        mRecyclerView = findViewById(R.id.rv_items);
+    }
 
     private void readCode(View view){
         }
@@ -96,7 +108,7 @@ public class HomeActivirty extends AppCompatActivity {
         int age = Integer.parseInt(mInpurtAge.getText().toString());
 
         // Create new object class of Person and add a value on model class
-        Person person = new Person(name,age);
+        User person = new User(name,age);
         String key = mRef.push().getKey();
         mRef.child(key).setValue(person);
         Toast.makeText(this,"Data is added",Toast.LENGTH_SHORT).show();
---
+++ app/src/main/java/com/example/realtimedatabasekotlin/User.java
@@ -0,0 +1,30 @@
+package com.example.realtimedatabasekotlin;
+
+public class User
+{
+        String name;
+        int age;
+
+    public User(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+    public User() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
